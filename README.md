0f089a56ea8f31fbcbb006460525825c68d6ed97
ankitmukhia <ankitmukhia157@gmail.com>
2025-11-30 22:09:59 +0530

    some changes

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-JSON Visualizer
+<h1 align="center">JSON Visualizer</h1>
+
+<a href="https://jsonvisualizerr.vercel.app/">
+    <img src="https://jsonvisualizerr.vercel.app/twitter-json.png" alt="JSON Visualizer">
+</a>
--- app/layout.tsx
+++ app/layout.tsx
@@ -10,9 +10,38 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "JSON Viewer",
-  description:
-    "Visualize and format JSON easily. Paste your data and explore it in a clean, readable structure.",
+  title: "JSON Viewer | AI Insights, format JSON easily",
+  description: "Visualize and format JSON easily. Paste your data and explore it in a clean, readable structure.",
+  metadataBase: new URL("https://jsonvisualizerr.vercel.app"),
+  openGraph: {
+    title: "JSON Viewer | AI Insights, format JSON easily",
+    type: "website",
+    siteName: "JSON Viewer",
+    url: "https://jsonvisualizerr.vercel.app",
+    description: "Visualize and format JSON easily. Paste your data and explore it in a clean, readable structure.",
+    locale: "en_US",
+    images: [
+      {
+        url: "https://jsonvisualizerr.vercel.app/og-json.png",
+        width: 1200,
+        height: 630,
+        alt: "JSON Viewer",
+      },
+    ],
+  },
+  twitter: {
+    title: "JSON Viewer | AI Insights, format JSON easily",
+    description: "Visualize and format JSON easily. Paste your data and explore it in a clean, readable structure.",
+    card: "summary_large_image",
+    images: [
+      {
+        url: "https://jsonvisualizerr.vercel.app/twitter-json.png",
+        width: 1200,
+        height: 630,
+        alt: "JSON Viewer",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
